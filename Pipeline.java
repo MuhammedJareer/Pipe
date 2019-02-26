d31bef4c3f40ec4f5f0d89debb40d57221ebd5b7
MuhammedJareer <39986950+MuhammedJareer@users.noreply.github.com>
2019-02-26 07:37:48 +0530

    Create Pipeline.java

---
+++ Pipeline.java
@@ -0,0 +1,24 @@
+Pipeline{
+agent any
+
+stages{
+stage('compile stage'){
+
+steps{
+withMaven(maven : 'maven_3_5_0'){
+sh 'mvn clean compile'
+}
+}
+}
+stage('testing stage'){
+
+steps{
+
+withMaven(maven : 'maven_3_5_0'){
+
+sh 'mvn clean compile'
+}
+}
+}
+}
+}
